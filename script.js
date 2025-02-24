80a3861f3a962090add4257e326f8843175011da
RichardJr <richardjunior_o_cara_@hotmail.com>
2025-02-24 19:53:55 -0300

    atualizando para ser pt-en

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
-function toggleMode() {
-  const html = document.documentElement
+document.addEventListener("DOMContentLoaded", function () {
+  const switchButton = document.querySelector("#switch button")
+  switchButton.addEventListener("click", toggleLanguage)
+  updateSwitchText()
+})
 
-  html.classList.toggle("light")
+function toggleLanguage() {
+  const html = document.documentElement
+  const isEnglish = html.classList.toggle("english")
+  updateSwitchText()
 
   const img = document.querySelector("#profile img")
+  const profileText = document.querySelector("#profile p")
+  const links = document.querySelectorAll("ul li a")
+  const footer = document.querySelector("footer")
 
-  if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/imagelight.png")
-    img.setAttribute("alt", "Foto Lucas Rangel de perfil modo light")
+  if (isEnglish) {
+    img.setAttribute("alt", "Profile picture of Lucas Rangel")
+    profileText.textContent = "@lucasrangel_01"
+    links[0].textContent = "About Me"
+    links[1].textContent = "View My Portfolio"
+    links[2].textContent = "Contact"
+    footer.innerHTML = `Made by <a href="mailto:richard.junior1601@hotmail.com">Richard Junior</a>`
   } else {
-    img.setAttribute("src", "./assets/imagedark.png")
-    img.setAttribute("alt", "Foto Lucas Rangel de perfil modo dark")
+    img.setAttribute("alt", "Foto de perfil de Lucas Rangel")
+    profileText.textContent = "@lucasrangel_01"
+    links[0].textContent = "Sobre mim"
+    links[1].textContent = "Ver meu portifólio"
+    links[2].textContent = "Contato"
+    footer.innerHTML = `Feito por <a href="mailto:richard.junior1601@hotmail.com">Richard Junior</a>`
   }
+}
 
-  //pegar tag img e se tiver light mode -> img light, se nn -> img normal
+function updateSwitchText() {
+  const switchButton = document.querySelector("#switch button")
+  const isEnglish = document.documentElement.classList.contains("english")
+  switchButton.textContent = isEnglish ? "EN" : "PT"
 }
